9e438bba1075317e57d147bceda1b2f60d393ba1
Tatsiana  Taranushchenko <tatsiana.taranushchenko@trustradius.com>
2023-01-10 19:13:51 +0300

    install express

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
+import { CssBaseline } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#6de8c0',
+		},
+		secondary: {
+			main: '#47beff',
+			light: '#a7e6ff',
+		},
+		background: {
+			paper: '#fff',
+		},
+		text: {
+			primary: '#222222',
+		},
+	},
+	typography: {
+		body2: {
+			fontSize: '0.75rem',
+			lineHeight: '1.5',
+		},
+	},
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
