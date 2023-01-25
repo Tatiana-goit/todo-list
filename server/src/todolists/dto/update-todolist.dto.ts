0571543a83016fb1690089583ed0aeb55247ec74
Tatsiana  Taranushchenko <tatsiana.taranushchenko@trustradius.com>
2023-01-26 00:30:03 +0300

    edited Create Todo List Page, change design for List View

--- server/src/todolists/dto/update-todolist.dto.ts
+++ server/src/todolists/dto/update-todolist.dto.ts
@@ -1,8 +1,3 @@
-// import { PartialType } from '@nestjs/mapped-types';
-// import { CreateTodolistDto } from './create-todolist.dto';
-
-// export class UpdateTodolistDto extends PartialType(CreateTodolistDto) {}
-
 import { IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateTodoDto } from './create-todo.dto';
